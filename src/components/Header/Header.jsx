523659cfbfba286bb626ca1fa76657a53d877b20
Mehedi Hasan Murad <mehedihasna500422@gamil.com>
2025-07-20 06:45:05 +0600

    blogs data added using useState and useEffect

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl font-semibold'>Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
